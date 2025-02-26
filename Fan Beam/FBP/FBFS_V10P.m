8d3097322be51f91e68254d7923d466ecd7a84fc
Yang Mou <131220915+SoapMou@users.noreply.github.com>
2025-02-26 16:07:05 +0800

    Add files via upload

--- Fan Beam/FBP/FBFS_V10P.m
+++ Fan Beam/FBP/FBFS_V10P.m
@@ -1,8 +1,8 @@
-%Fan-Beam FBP Interpolation V4 Parfor
+%Fan-Beam FBP Siddon V10 Parfor
 %----------------------------------Description---------------------------------%
-%系统矩阵:Interpolation
+%系统矩阵:Siddon
 %重建算法:FBP
-%基础程序:FBIAV3Par
+%基础程序:FBFS_V9P
 %------------------------------------------------------------------------------%
 tic  %开始计时
 %----------------------------------Parameters----------------------------------%
@@ -10,12 +10,12 @@ Gdet  = 'A';   %探测器排列方式(A=等角;L=等距)
 Gpix  = 512;   %图像尺寸(单边像素数量)
 Gsod  = 726;   %射线源-旋转中心距离(sod>0.5*pix/(sin(fan/2)*sin(45°)))
 Gfan  = 60;    %扇形束张角(角度制)
-Ganum = 1200;  %旋转角度数量(射线源沿逆时针方向旋转)
+Ganum = 2400;  %旋转角度数量(射线源沿逆时针方向旋转)
 Gdnum = 801;   %探测器数量(探测器沿逆时针方向编号)
 Gfidx = 1;     %滤波器编号(1=Ramp;2=Hanning;3=Hamming;4=Cosine)
 Gimg  = phantom(Gpix);  %重建对象
 %----------------------------------File Name-----------------------------------%
-FN = strcat('FBFI_V4',Gdet,'p',string(Gpix),'s',string(Gsod),'f',string(Gfan*10),...
+FN = strcat('FBFS_V10',Gdet,'p',string(Gpix),'s',string(Gsod),'f',string(Gfan*10),...
             'a',string(Ganum),'d',string(Gdnum),'f',string(Gfidx));
 %------------------------------------------------------------------------------%
 GFBP  = zeros(Gpix*Gpix,1);  %FBP数据
@@ -32,58 +32,79 @@ win(2,:) = hann(Gflen);
 win(3,:) = hamming(Gflen);
 win(4,:) = cos(pi*linspace(-1,1,Gflen)/2);
 Gfilt = transpose(ifftshift(abs(linspace(-1,1,Gflen)).*win(Gfidx,:)));  %预先ifftshift
-%--------------------------------Image meshgrid--------------------------------%
-Gms = (Gpix-1)/2;  %meshgrid shift(图像网格偏移量)
-[GIX,GIY] = meshgrid(-Gms:1:Gms,Gms:-1:-Gms);  %重建对象的XY坐标网格
-%---------------------------Initial fan-beam meshgrid--------------------------%
-Gfms = ceil(sqrt(2)*Gpix/2)+0.5;  %射线网格偏移量
-Gfw  = 2*Gfms+1;  %射线网格宽度(每条射线的采样点数量)
-GFX  = (repmat(-Gfms:1:Gfms,Gdnum,1)-Gsod).*cosd(Giba)+Gsod;  %初始状态射线的X坐标网格
-GFY  = tand(Giba).*(GFX-Gsod);  %初始状态射线的Y坐标网格
 %-------------------------------FBP at each angle------------------------------%
 parfor a = 1:Ganum
     %--------全局变量->局部变量--------%
     pix  = Gpix;
-    ms   = Gms;
-    fw   = Gfw;
-    FX   = GFX;
-    FY   = GFY;
+    dnum = Gdnum;
+    img  = Gimg;
     sa   = (a-1)*360/Ganum;  %source angle(角度制)
+    ba   = Giba+sa;  %beam angle(角度制)
+    x0   = Gsod*cosd(sa);  %射线源的X轴坐标
+    y0   = Gsod*sind(sa);  %射线源的Y轴坐标
     %--------其他局部变量--------%
+    hp   = pix/2;
     FBP  = zeros(pix*pix,1);  %FBP数据
     mask = zeros(pix*pix,1);  %背景区域(背景区域=1,目标区域=0)
-    %--------Projection of each beam--------%
-    X = cosd(sa)*FX-sind(sa)*FY;  %射线的X坐标网格(逆时针旋转)
-    Y = sind(sa)*FX+cosd(sa)*FY;  %射线的Y坐标网格(逆时针旋转)
-    proj = interp2(GIX,GIY,Gimg,X,Y,'linear');  %使用线性插值法计算投影数据
-    proj(isnan(proj)) = 0;  %将nan赋值0
-    PD = sum(proj,2);  %将proj每行数据累加得到每条射线的投影值
-    %--------Filtering/Backprojection--------%
-    fftPD = real(ifft(fft(PD,Gflen).*Gfilt));  %滤波
-    fPD   = fftPD(1:Gdnum);
-    for r = 1:Gdnum
-        for c = 1:fw
-            if abs(X(r,c)) <= ms && abs(Y(r,c)) <= ms
-                dx = X(r,c)+ms-floor(X(r,c)+ms);  %插值点与左侧X坐标的差
-                ct = ceil(X(r,c)+ms)+1;  %column top,插值点右侧对应图像的列
-                cb = floor(X(r,c)+ms)+1;  %column bottom,插值点左侧对应图像的列
-                dy = Y(r,c)+ms-floor(Y(r,c)+ms);  %插值点与下方Y坐标的差
-                rt = pix-ceil(Y(r,c)+ms);  %row top,插值点上方对应图像的行
-                rb = pix-floor(Y(r,c)+ms);  %row bottom,插值点下方对应图像的行
-                FBP((ct-1)*pix+rt) = FBP((ct-1)*pix+rt)+dx*dy*fPD(r);  %右上
-                FBP((cb-1)*pix+rt) = FBP((cb-1)*pix+rt)+(1-dx)*dy*fPD(r);  %左上
-                FBP((ct-1)*pix+rb) = FBP((ct-1)*pix+rb)+dx*(1-dy)*fPD(r);  %右下
-                FBP((cb-1)*pix+rb) = FBP((cb-1)*pix+rb)+(1-dx)*(1-dy)*fPD(r);  %左下
-                if PD(r) == 0  %投影值=0,记录背景区域
-                    mask((ct-1)*pix+rt) = 1;  %右上点
-                    mask((cb-1)*pix+rt) = 1;  %左上点
-                    mask((ct-1)*pix+rb) = 1;  %右下点
-                    mask((cb-1)*pix+rb) = 1;  %左下点
-                end
+    SIDn = zeros(dnum,1);  %Siddon number(射线穿过像素的数量)
+    SIDi = zeros(pix*2-1,dnum);  %Siddon index(射线穿过像素的编号)
+    SIDw = ones(pix*2-1,dnum);  %Siddon weight(射线穿过像素的权重)(默认=1)
+    PD   = zeros(dnum,1);  %投影数据
+    COS  = abs(cosd(ba));
+    %--------Projection--------%
+    for d = 1:dnum
+        k = tand(ba(d));  %射线的斜率
+        if abs(k) == inf  %--------射线垂直于X轴--------%
+            if abs(x0) <= hp  %判断射线是否穿过图像
+                col = ceil(x0)+hp;  %确定射线对应的列
+                SIDn(d) = pix;  %记录像素的数量
+                SIDi(1:pix,d) = (col-1)*pix+1:1:col*pix;  %记录像素的编号
+                PD(d) = sum(img(:,col));  %记录射线的投影值
+            end 
+        elseif k == 0  %--------射线垂直于Y轴--------%
+            if abs(y0) <= hp  %判断射线是否穿过图像
+                row = -floor(y0)+hp;  %确定射线对应的行
+                SIDn(d) = pix;  %记录像素的数量
+                SIDi(1:pix,d) = row:pix:(pix-1)*pix+row;  %记录像素的编号
+                PD(d) = sum(img(row,:));  %记录射线的投影值
+            end
+        else  %--------射线处于其他角度--------%
+            xT = (hp-y0)/k+x0;  %x top
+            xB = (-hp-y0)/k+x0;  %x bottom
+            if (xT<-hp && xB<-hp)+(xT>hp && xB>hp) ~= 0  %射线是否从图像左/右侧经过
+                continue;
+            end
+            if k < 0  %射线与图像左/右交点X坐标
+                X = [max(-hp,xT),min(hp,xB)];
+            else
+                X = [max(-hp,xB),min(hp,xT)];
+            end
+            Y = (X-x0)*k+y0;  %射线与图像左/右交点Y坐标
+            xpos = unique([X,ceil(X(1)):floor(X(2)),((ceil(min(Y)):floor(max(Y)))-y0)/k+x0]);  %所有交点的X坐标
+            SIDn(d) = length(xpos)-1;  %射线穿过像素的数量
+            for n = 1:SIDn(d)
+                row = hp-floor((xpos(n)+xpos(n+1))*k/2-x0*k+y0);  %当前像素的行
+                col = hp+1+floor(xpos(n));  %当前像素的列(xpos从小到大,简化计算)
+                SIDi(n,d) = (col-1)*pix+row;  %记录像素的编号
+                SIDw(n,d) = (xpos(n+1)-xpos(n))/COS(d);  %记录像素的权重
+                PD(d) = PD(d)+img(row,col)*SIDw(n,d);  %记录射线的投影值
             end
         end
     end
-    GFBP = GFBP+FBP;  %将当前角度的FBP数据累加到GFBP中
+    %--------Filtering/Backprojection--------%
+    fftPD = real(ifft(fft(PD,Gflen).*Gfilt));  %滤波
+    fPD   = fftPD(1:dnum);
+    for d = 1:dnum
+        if SIDn(d) ~= 0  %判断射线是否穿过图像
+            for n = 1:SIDn(d)  %反投影
+                FBP(SIDi(n,d)) = FBP(SIDi(n,d))+fPD(d)*SIDw(n,d);
+            end
+            if PD(d) == 0  %记录背景区域
+                mask(SIDi(1:SIDn(d),d)) = 1;
+            end
+        end
+    end
+    GFBP  = GFBP+FBP;  %将当前角度的FBP数据累加到GFBP中
     Gmask = Gmask+mask;  %将当前角度的mask累加到Gmask中
 end
 %-----------------------Normalization and Reconstruction-----------------------%
